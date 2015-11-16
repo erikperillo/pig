05fa16850638191f6f00f01f8a7ca68633108f88
Erik Perillo <erik.perillo@gmail.com>
2015-11-16 09:05:39 -0200

    more order on sctructure

--- pig/devices.py
+++ pig/devices.py
@@ -1,6 +1,6 @@
-from base import *
+from . import base
 
-class Motor(OutputGadget):
+class Motor(base.OutputGadget):
     """interface that represents motors."""    
     def move(intensity: float) -> None:
         """moves the motor with the speed defined by intensity.
@@ -8,20 +8,21 @@ class Motor(OutputGadget):
            in reverse direction and 1 is the maximum speed in normal direction."""
         raise NotImplementedError("method must be implemented to comply with the interface")
 
-class LightSource(OutputGadget):
+class LightSource(base.OutputGadget):
     """interface that represents some light source."""    
     def lighten(intensity: float) -> None:
         """produces light via the sensor with luminosity defined by intensity.
            intensity is a value between [0,1], whereas 0 is the mininum possible 
            luminosity and 1 is the maximum possible luminosity."""
+        raise NotImplementedError("method must be implemented to comply with the interface")
 
-class Button(InputGadget):
+class Button(base.InputGadget):
     """interface that represents a simple button."""    
     def pressed() -> bool:
         """returns True if the button is/was held down and False otherwise."""
         raise NotImplementedError("method must be implemented to comply with the interface")
 
-class FlameThrower(OutputGadget):
+class FlameThrower(base.OutputGadget):
     """interface that represents flamethrower devices."""
     def fire(intensity: float) -> None:
         """sends flaming death to the world.
@@ -29,7 +30,7 @@ class FlameThrower(OutputGadget):
            fire intensity and 1 is the maximum."""
         raise NotImplementedError("method must be implemented to comply with the interface")
 
-class DistanceSensor(ScalarSensor):
+class DistanceSensor(base.ScalarSensor):
     """interface that represents distance sensors. it may be an ultrassonic or infrared one,
        for example."""    
     def getDistance() -> float:
@@ -37,7 +38,7 @@ class DistanceSensor(ScalarSensor):
            greater or equal to zero."""
         raise NotImplementedError("method must be implemented to comply with the interface")
 
-class LuminositySensor(ScalarSensor):
+class LuminositySensor(base.ScalarSensor):
     """interface that represents a light sensor."""
     def getLuminosity() -> float:
         """gets the luminosity read by the sensor. the return value must be between [0,1], 
@@ -45,7 +46,7 @@ class LuminositySensor(ScalarSensor):
            and 1 is the maximum."""
         raise NotImplementedError("method must be implemented to comply with the interface")
 
-class GPS(Receiver):
+class GPS(base.Receiver):
     """interface that represents a GPS receiver device."""
     def getNMEAMessage() -> str:
         """gets a standard NMEA message from device.""" 
